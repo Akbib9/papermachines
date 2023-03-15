829c5c1cadd1069181744c11b20979fc17545b9b
Christian Frisson <christian.frisson@gmail.com>
2023-03-15 09:30:04 -0400

    chrome/content/papermachines/include.js: update scripts loading API

--- chrome/content/papermachines/include.js
+++ chrome/content/papermachines/include.js
@@ -1,8 +1,10 @@
 if (!Zotero.PaperMachines) {
 	const pmLoader = Components.classes["@mozilla.org/moz/jssubscript-loader;1"]
 					.getService(Components.interfaces.mozIJSSubScriptLoader);
-	pmLoader.loadSubScript("chrome://papermachines/content/papermachines.js");
-	Zotero.PaperMachines.init();
+	// pmLoader.loadSubScript("chrome://papermachines/content/papermachines.js");
+	var scripts = ['Preferences','papermachines']; // ,'strptime'
+    scripts.forEach(s => pmLoader.loadSubScript('chrome://papermachines/content/' + s + '.js'));
+	// Zotero.PaperMachines.init();
 }
 
 Zotero.PaperMachines.createUI();
